03759ba9a99c3b92a4ed687ae39234a554ab0d89
Victor Hyltoft <63429024+victorhyltoft@users.noreply.github.com>
2022-01-03 15:39:38 +0100

    Player model

--- src/main/java/com/example/gorilla/Models/Player.java
+++ src/main/java/com/example/gorilla/Models/Player.java
@@ -1,4 +1,23 @@
 package com.example.gorilla.Models;
 
+import javafx.geometry.Point2D;
+
 public class Player {
+    public String name;
+    public Point2D location;
+    public int wins;
+    public String texture;
+
+    // Constructor 1 (Multiple parameters)
+    public Player(String name, Point2D location, String texture) {
+        this.name = name;
+        this.location = location;
+        this.texture = texture;
+        this.wins = 0;
+    }
+
+    // Constructor 2
+    public Player(String name) {
+        this.name = name;
+    }
 }
