9545115b254f35967084b4383cb84b6195c7b29d
Triids <mikkel.allermand@icloud.com>
2022-01-03 22:06:33 +0100

    Controller class for main menu Added functionality to check for clicks on the buttons, as well as simple methods: Exit, exits the game Play game: Takes you to the page where you insert the parameters for the game Methods to detect textfields to input these parameters A ready button that for now just calls the above methods to check textfields.

---
+++ src/main/java/com/example/gorilla/Controller.java
@@ -0,0 +1,48 @@
+package com.example.gorilla;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Controller {
+    @FXML
+    private TextField heightField;
+    @FXML
+    private TextField widthField;
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+    public void exit(ActionEvent e) {
+        System.exit(0);
+    }
+
+    public int getWidth() {
+        return Integer.parseInt(widthField.getText());
+
+    }
+
+    public int getHeight() {
+        return Integer.parseInt(heightField.getText());
+
+    }
+
+    public void startGame(ActionEvent e) {
+        System.out.println(getWidth()+"x"+getHeight());
+
+    }
+
+    public void switchToPlayGameScreen(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("playGameScreen.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+}
