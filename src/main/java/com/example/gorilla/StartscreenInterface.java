946c0388ffdb0af1fe4848eabac9d34d9bf7b0b5
Triids <mikkel.allermand@icloud.com>
2022-01-03 22:03:29 +0100

    Added startscreen fxml and java files

---
+++ src/main/java/com/example/gorilla/StartscreenInterface.java
@@ -0,0 +1,22 @@
+package com.example.gorilla;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class StartscreenInterface extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("startscreen.fxml"));
+        Scene scene = new Scene(root);
+
+        stage.setScene(scene);
+        stage.show();
+    }
+}
