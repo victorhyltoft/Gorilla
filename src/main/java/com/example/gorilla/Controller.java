25ced06ccdf6cd9beeb6250c4615a4a668a47b0f
Victor Hyltoft <63429024+victorhyltoft@users.noreply.github.com>
2022-01-05 16:35:09 +0100

    Refactoring

--- src/main/java/com/example/gorilla/Controller.java
+++ src/main/java/com/example/gorilla/Controller.java
@@ -2,6 +2,7 @@ package com.example.gorilla;
 
 import com.example.gorilla.Models.Player;
 import com.example.gorilla.Models.Projectile;
+import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -18,7 +19,7 @@ import java.io.IOException;
 import java.util.Objects;
 import java.util.Random;
 
-public class GameController {
+public class Controller extends Application {
     @FXML
     private TextField angleField;
     @FXML
@@ -36,6 +37,61 @@ public class GameController {
     private int height;
     private Stage stage;
     private Scene scene;
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    /**
+     *
+     */
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("startscreen.fxml")));
+        Scene scene = new Scene(root);
+        stage.setResizable(false);
+        stage.setScene(scene);
+        stage.setTitle("SIMPGorillas");
+        stage.show();
+    }
+
+    /**
+     *
+     */
+    public void startGame2(ActionEvent event) throws IOException {
+        System.out.println(getWidth()+"x"+getHeight());
+        System.out.println(getGravity());
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("baseLevel.fxml")));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root,getWidth(),getHeight());
+        stage.setScene(scene);
+        stage.setResizable(false);
+        stage.show();
+    }
+
+    /**
+     *
+     */
+    public void switchToPlayGameScreen(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("playGameScreen.fxml")));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    /**
+     *
+     */
+    public void updateGame(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("baseLevel.fxml")));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        Group trajectory = Trajectory.trajectoryToGroup(bananaThrow());
+        ((AnchorPane) root).getChildren().add(trajectory);
+        scene = new Scene(root,width,height);
+        stage.setScene(scene);
+        stage.show();
+    }
 
     public int coinFlip() {
         Random random = new Random();
@@ -85,35 +141,6 @@ public class GameController {
 
     public double getGravity() {
         return Double.parseDouble(gravityField.getText());
-    }
-
-    public void startGame(ActionEvent event) throws IOException {
-        System.out.println(getWidth()+"x"+getHeight());
-        System.out.println(getGravity());
-        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("baseLevel.fxml")));
-        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-        scene = new Scene(root,getWidth(),getHeight());
-        stage.setScene(scene);
-        stage.setResizable(false);
-        stage.show();
-    }
-
-    public void switchToPlayGameScreen(ActionEvent event) throws IOException {
-        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("playGameScreen.fxml")));
-        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-        scene = new Scene(root);
-        stage.setScene(scene);
-        stage.show();
-    }
-
-    public void updateGame(ActionEvent event) throws IOException {
-        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("baseLevel.fxml")));
-        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-        Group trajectory = Trajectory.trajectoryToGroup(bananaThrow());
-        ((AnchorPane) root).getChildren().add(trajectory);
-        scene = new Scene(root,width,height);
-        stage.setScene(scene);
-        stage.show();
     }
 
    /* public void updateGame(Stage stage, Group root, Projectile projectile) {
