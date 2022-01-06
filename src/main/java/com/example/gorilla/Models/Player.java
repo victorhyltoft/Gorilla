9e53735b559844c9591f06dff1f2bbea6ddd92e1
Victor Hyltoft <63429024+victorhyltoft@users.noreply.github.com>
2022-01-06 15:49:03 +0100

    Setter for name in Player

--- src/main/java/com/example/gorilla/Models/Player.java
+++ src/main/java/com/example/gorilla/Models/Player.java
@@ -5,13 +5,17 @@ import javafx.geometry.Point2D;
 public class Player {
     public String name;
     public Point2D location;
-    public int score = 0;
+    public int score;
 
     // Constructor 1
     public Player(String name, Point2D location) {
         this.name = name;
         this.location = location;
         this.score = 0;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 
     public void incrementScore() {
